96f97b8668f0b08bd8dfd6a46dc3b34a87c4af1d
Nguyen Phat <phat.nguyennpk1311@hcmut.edu.vn>
2023-12-07 12:51:06 +0700

    v1

--- server/index.js
+++ server/index.js
@@ -10,16 +10,15 @@ import cartRoutes from './routes/cart.js';
 
 const app = express();
 dotenv.config();
-
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
-app.use('/products', productRoutes);
-app.use('/cart', cartRoutes);
+const base = '/api/v1';
+app.use(base +'/products', productRoutes);
+app.use(base+ '/cart', cartRoutes);
 
-
-const PORT = process.env.PORT || 50001;
+const PORT = process.env.PORT || 5001;
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true 
 }).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
