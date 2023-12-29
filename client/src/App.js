0bcf4750030033160685f5ac980a452223d516e4
Phat Nguyen <phatnguyen@Phats-MacBook-Pro.local>
2023-12-29 12:42:11 +0700

    change to another

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
-import Cart from 'components/Cart';
-import Products from 'components/Products';
-import { useDispatch } from 'react-redux';
-import { setProducts } from 'state/index.js';
-import { setCart} from 'state/cart.js';
-import { getAllProducts } from 'api/productApi.js';
-import { getCartItems } from 'api/cartApi.js';
-import './app.scss'
+import React, { useEffect } from "react";
+import Cart from "components/Cart";
+import Products from "components/Products";
+import { useDispatch } from "react-redux";
+import { setProducts } from "state/index.js";
+import { setCart } from "state/cart.js";
+import { getAllProducts } from "api/productApi.js";
+import { getCartItems } from "api/cartApi.js";
+import "./app.scss";
+import background from "assets/background.mp4";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,32 +15,35 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getAllProducts();
-      if (data){
-        console.log('products: ', data);
+      if (data) {
+        console.log("products: ", data);
         dispatch(setProducts(data));
-      }      
-    }
+      }
+    };
     const fetchCart = async () => {
       const data = await getCartItems();
-      if (data){
+      if (data) {
         dispatch(setCart(data));
-        console.log('cart: ', data);
+        console.log("cart: ", data);
       }
-    }
+    };
 
     fetchData();
     fetchCart();
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
   return (
     <>
       <div className="containerApp">
-        <div className="containerApp__left">
+        {/* <div className="containerApp__left">
           <Products />
         </div>
         <div className="containerApp__left">
           <Cart />
-        </div>
+        </div> */}
+        <video className="videoTag" autoPlay loop muted>
+          <source src={background} type="video/mp4" />
+        </video>
       </div>
     </>
   );
